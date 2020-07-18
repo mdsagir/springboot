381543f00736432fdaf96e05715d720abde34fcb
mdsagir <tech.sagir@gmail.com>
2020-07-18 21:08:59 +0530

    added spring-boot auth resource keyclock example

---
+++ springboot-keycloak-resource-server/src/main/java/com/greenjava/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.greenjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+}
